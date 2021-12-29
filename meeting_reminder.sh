e51c76f0af5a6aaf58002c304300f5aa8d75fade
Ahmad-mufied <ahmadmufied.work@gmail.com>
2021-12-30 00:23:06 +0700

    Making meeting reminder script

---
+++ meeting_reminder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+meeting_info=$(zenity --forms \
+  --title 'Meeting' --text 'Reminder information' \
+  --add-calender 'Date' --add-entry 'Title' \
+  --add-entry 'Emails' \
+  2>/def/null)
+
+if [[-n "$meeting_info"]]: then
+  python3 send_remainders.py "$meeting_info"
+fi
